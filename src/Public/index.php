b52fcbecf6048771383f2d3c4a76d2776a5ca85f
KatherineHarper <42791962+KatherineHarper@users.noreply.github.com>
2020-10-08 20:30:41 +1300

    Added virtual host

---
+++ src/Public/index.php
@@ -0,0 +1,16 @@
+<html>
+<body>
+
+<h1>Developer News</h1>
+
+<?php echo "The Best PHP Examples"; 
+
+
+/*
+ * This goes up one level out of /public
+ * and goes into /App to find bootstrap.php
+ */
+require(dirname(__FILE__).'/../App/bootstrap.php');?>
+
+</body>
+</html> 
